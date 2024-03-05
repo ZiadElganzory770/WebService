1d97dca39b8734980729d7b42ff7385667c7d2fb
Ziad Mohamed Elganzory <ziadmohamed770@outlook.com>
2024-03-05 20:51:40 +0200

    Lab1

---
+++ Lab1/config.php
@@ -0,0 +1,5 @@
+<?php
+define("__JSON_FILE__","city.list.json");
+define("__API_URL__" ,"https://api.openweathermap.org/data/2.5/weather?id=");
+define("__API_KEY__" ,"2f35751f2c892a1501f35cf254252aea");
+?>
